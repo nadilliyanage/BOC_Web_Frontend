c4128ef1b4031eebbdaf39b85d168dbb3a4c39a4
Nadil <liyanage.nadil@gmail.com>
2025-03-17 11:46:38 +0530

    navigations
    navigations

--- src/pages/Home/Components/Card.jsx
+++ src/pages/Home/Components/Card.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const Card = ({ title, count, icon: Icon, iconColor }) => {
+const Card = ({ title, count, icon: Icon, iconColor, onClick }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
     <Paper
       elevation={5}
-      className={`w-1/5 h-32 bg-white shadow-lg m-4 rounded-md   flex items-center justify-center  dark:bg-dark_1 transition-transform hover:scale-105`}
+      className={`w-1/5 h-32 bg-white shadow-lg m-4 rounded-md flex items-center justify-center dark:bg-dark_1 transition-transform hover:scale-105 cursor-pointer`}
+      onClick={handleClick}
     >
       <div className="flex flex-row-reverse justify-center items-center">
         <div className="flex flex-col mx-6">
@@ -15,7 +25,7 @@ const Card = ({ title, count, icon: Icon, iconColor }) => {
           </label>
         </div>
         <div
-          className={`p-6 bg-white dark:bg-dark_3 rounded-full shadow-lg hover:transition-opacity `}
+          className={`p-6 bg-white dark:bg-dark_3 rounded-full shadow-lg hover:transition-opacity`}
         >
           <Icon className={`text-3xl ${iconColor}`} />
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,37 @@
-// Home.jsx
 import React, { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { MdAccessTimeFilled, MdReviews } from "react-icons/md";
 import { IoTimer } from "react-icons/io5";
 import { BiSolidMessageSquareError } from "react-icons/bi";
 import MessageCountChart from "./Components/MessageCountChart";
-import Card from "./Components/Card"; // Import the Card component
+import Card from "./Components/Card";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const [userCount, setUserCount] = useState(0); // State to store the user count
+  const [userCount, setUserCount] = useState(0);
   const [pendingMessageCount, setPendingMessageCount] = useState(0);
   const [scheduledMessageCount, setScheduledMessageCount] = useState(0);
   const [toReviewMessageCount, setToReviewMessageCount] = useState(0);
   const [errorMessageCount, setErrorMessageCount] = useState(0);
+  const navigate = useNavigate();
 
   // Fetch the user count from the backend API
   useEffect(() => {
     const fetchUserCount = async () => {
       try {
-        const response = await fetch("http://localhost:8080/api/v1/user-count"); // Replace with your backend URL
+        const response = await fetch("http://localhost:8080/api/v1/user-count");
         if (!response.ok) {
           throw new Error("Failed to fetch user count");
         }
         const data = await response.json();
-        setUserCount(data); // Update the state with the user count
+        setUserCount(data);
       } catch (error) {
         console.error("Error fetching user count:", error);
       }
     };
 
     fetchUserCount();
-  }, []); // Empty dependency array ensures this runs only once on component mount
+  }, []);
 
   // Fetch the pending message count from the backend API
   useEffect(() => {
@@ -38,12 +39,12 @@ const Home = () => {
       try {
         const response = await fetch(
           "http://localhost:8080/api/v1/send-message/pending-sms-count"
-        ); // Replace with your backend URL
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch pending message count");
         }
         const data = await response.json();
-        setPendingMessageCount(data); // Update the state with the pending message count
+        setPendingMessageCount(data);
       } catch (error) {
         console.error("Error fetching pending message count:", error);
       }
@@ -58,12 +59,12 @@ const Home = () => {
       try {
         const response = await fetch(
           "http://localhost:8080/api/v1/send-message/scheduled-sms-count"
-        ); // Replace with your backend URL
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch scheduled message count");
         }
         const data = await response.json();
-        setScheduledMessageCount(data); // Update the state with the scheduled message count
+        setScheduledMessageCount(data);
       } catch (error) {
         console.error("Error fetching scheduled message count:", error);
       }
@@ -78,12 +79,12 @@ const Home = () => {
       try {
         const response = await fetch(
           "http://localhost:8080/api/v1/create-message/to-review-sms-count"
-        ); // Replace with your backend URL
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch ToReview message count");
         }
         const data = await response.json();
-        setToReviewMessageCount(data); // Update the state with the ToReview message count
+        setToReviewMessageCount(data);
       } catch (error) {
         console.error("Error fetching ToReview message count:", error);
       }
@@ -98,12 +99,12 @@ const Home = () => {
       try {
         const response = await fetch(
           "http://localhost:8080/api/v1/send-message/error-sms-count"
-        ); // Replace with your backend URL
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch Error message count");
         }
         const data = await response.json();
-        setErrorMessageCount(data); // Update the state with the ToReview message count
+        setErrorMessageCount(data);
       } catch (error) {
         console.error("Error fetching ToReview message count:", error);
       }
@@ -112,17 +113,29 @@ const Home = () => {
     fetchToReviewMessageCount();
   }, []);
 
+  // Handler for Pending SMS card click
+  const handlePendingSMS = () => {
+    navigate("/reports", { state: { activeTab: "pendingMessage" } });
+  };
+
+  // Handler for Scheduled SMS card click
+  const handleScheduledSMS = () => {
+    navigate("/reports", { state: { activeTab: "scheduleMessage" } });
+  };
+
+  // Handler for Error SMS card click
+  const handleErrorSMS = () => {
+    navigate("/reports", { state: { activeTab: "errorMessage" } });
+  };
+
+  // Handler for Review Message
+  const handleReviewSMS = () => {
+    navigate("/createMessage", { state: { activeTab: "msgReview" } });
+  };
+
   return (
     <div className="p-4 m-4 bg-primary_2 dark:bg-dark_2">
       <div className="flex flex-wrap items-center justify-center">
-        {/* All Users Card */}
-        {/* <Card
-          title="All Users"
-          count={userCount}
-          icon={FaUser}
-          bgColor="bg-white"
-          iconColor="text-blue-600"
-        /> */}
         {/* Pending SMS Card */}
         <Card
           title="Pending SMS"
@@ -130,6 +143,7 @@ const Home = () => {
           icon={MdAccessTimeFilled}
           bgColor="bg-white"
           iconColor="text-yellow-500"
+          onClick={handlePendingSMS}
         />
         {/* Scheduled SMS Card */}
         <Card
@@ -138,6 +152,7 @@ const Home = () => {
           icon={IoTimer}
           bgColor="bg-white"
           iconColor="text-green-500"
+          onClick={handleScheduledSMS}
         />
         {/* Error SMS Card */}
         <Card
@@ -146,6 +161,7 @@ const Home = () => {
           icon={BiSolidMessageSquareError}
           bgColor="bg-white"
           iconColor="text-red-600"
+          onClick={handleErrorSMS}
         />
         {/* SMS to Review Card */}
         <Card
@@ -154,6 +170,7 @@ const Home = () => {
           icon={MdReviews}
           bgColor="bg-white"
           iconColor="text-purple-600"
+          onClick={handleReviewSMS}
         />
       </div>
       <MessageCountChart />
